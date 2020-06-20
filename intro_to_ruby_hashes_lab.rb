afb1ca4d45adf513420831a6a94dbb5c872270e4
Alex Ngundji <alexngundji@gmail.com>
2020-06-20 19:14:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -69,7 +69,7 @@ def monopoly_with_fourth_tier
       :four_pieces_owned => 200,
       
     },
-      :names => {:reading_railroad => {:mortgage_value => 100 },
+      :names => {:reading_railroad => {:mortgage_value => },
         :pennsylvania_railroad =>{},
         :b_and_o_railroad =>{},
         :shortline_railroad =>{},
